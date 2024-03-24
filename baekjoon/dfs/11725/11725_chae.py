790b9ce1a4b5b3d1a1ec28a71cf7d4914c144767
원채령 <fairy3530@naver.com>
2024-03-24 13:46:18 +0900

    feat: sol(11725)

---
+++ baekjoon/dfs/11725/11725_chae.py
@@ -0,0 +1,22 @@
+import sys
+N = int(sys.stdin.readline())
+graph = [[] for _ in range(N + 1)]  # 양방향 그래프
+for i in range(N - 1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+visited = [False] * (N + 1)
+nodes = [0] * (N + 1)  # 부모 노드를 저장할 리스트
+
+
+def dfs(start):
+    visited[start] = True
+    for i in graph[start]:
+        if not visited[i]:
+            nodes[i] = start  # 현재 노드의 부모를 저장
+            dfs(i)
+
+print(graph)
+dfs(1)  # 루트가 1
+for i in range(2, N + 1):  # 2번 노트부터 출력
+    print(nodes[i])
