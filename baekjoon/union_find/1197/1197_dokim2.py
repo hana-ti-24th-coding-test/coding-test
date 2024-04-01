f2137ebf800c56cd2e6020f06021549c5cd4fa8b
KIM0212DH <godqhr803@naver.com>
2024-04-01 16:46:51 +0900

    feat: add 1197

---
+++ baekjoon/union_find/1197/1197_dokim2.py
@@ -0,0 +1,42 @@
+import sys
+
+v = int(sys.stdin.readline())
+e = int(sys.stdin.readline())
+board = [[] for _ in range(v + 1)]
+connections = []
+for _ in range(e):
+    a, b, c = map(int, sys.stdin.readline().split())
+    connections.append([a, b, c])
+
+connections.sort(key=lambda x: x[2])
+print(connections)
+parent = [i for i in range(v + 1)]
+print(parent)
+
+
+def find_parent(parent, x):
+    if x == parent[x]:
+        return x
+    parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+cost = 0
+for a, b, c in connections:
+    if find_parent(parent, a) != find_parent(parent, b):
+        print(parent)
+        print(a, b, c)
+        union_parent(parent, a, b)
+        cost += c
+print(parent)
+print(cost)
