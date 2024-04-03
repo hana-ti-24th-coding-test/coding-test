71a1994646652ce8dda2799bc6afd70050eede0e
채채 <109331665+cherrytonic@users.noreply.github.com>
2024-04-03 14:05:26 +0900

    feat: sol 1674

--- baekjoon/topological_sort/2252/2252_chae.py
+++ baekjoon/topological_sort/2252/2252_chae.py
@@ -24,18 +24,18 @@ def topological_sort():
             queue.append(i)
     # 큐가 빌 때까지 반복
     while queue:
-         # 큐에서 원소 꺼내기
-         now = queue.popleft()
-         result.append(now)
-         # 해당 원소와 연결된 노드들의 진입차수에서 1 빼기
-         for i in graph[now]:
-             indegree[i] -= 1
-             # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
-             if indegree[i] == 0:
-                 queue.append(i)
+        # 큐에서 원소 꺼내기
+        now = queue.popleft()
+        result.append(now)
+        # 해당 원소와 연결된 노드들의 진입차수에서 1 빼기
+        for i in graph[now]:
+            indegree[i] -= 1
+            # 새롭게 진입차수가 0이 되는 노드를 큐에 삽입
+            if indegree[i] == 0:
+                queue.append(i)
     # 위상 정렬을 수행한 결과 출력
     for i in result:
         print(i, end=' ')
 
-topological_sort()
 
+topological_sort()
