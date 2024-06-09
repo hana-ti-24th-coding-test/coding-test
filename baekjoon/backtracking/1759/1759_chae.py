d87667d81c8534f9f0e1d4c564183834d3dc5953
원채령 <fairy3530@naver.com>
2024-06-09 23:29:37 +0900

    add: readme

--- baekjoon/backtracking/1759/1759_chae.py
+++ baekjoon/backtracking/1759/1759_chae.py
@@ -1,24 +1,28 @@
 import sys
 L, C = map(int, sys.stdin.readline().split())
-list = sorted(sys.stdin.readline().split())
+alphabets = sorted(sys.stdin.readline().split())
 ans = []
-# n(index), cnt(모음의 개수), tst(완성되는 비밀번호 문자)
-def dfs(n, cnt, tst):
-    if n == C:  # 모든 알파벳의 사용 여부를 선택한 경우 종료
-        # 비밀번호 길이, 모음개수 >=1, 자음개수>= 2
-        if len(tst) == L and cnt >= 1 and L - cnt >= 2:
-            ans.append(tst)
+vowels = "aeiou"
+
+def dfs(n, cnt, password):
+    # n : 비밀번호 단어의 인덱스
+    if n == C:
+        # 비밀번호 길이 체크 및 모음(cnt)과 자음 개수 조건 체크
+        if len(password) == L and cnt >= 1 and L - cnt >= 2:
+            ans.append(password)
         return
-    dfs(n+1, cnt + tbl[ord(list[n])], tst + list[n]) # 모음 포함하는 경우
-    dfs(n+1, cnt, tst)  # 포함하지 않는 경우
+    # 현재 문자를 포함하는 경우
+    if alphabets[n] in vowels:  # 모음인 경우
+        dfs(n + 1, cnt + 1, password + alphabets[n])
+    else:
+        dfs(n + 1, cnt, password + alphabets[n])
+    # 현재 문자를 포함하지 않는 경우
+    dfs(n + 1, cnt, password)
 
 
-# 모음인 경우 1, 그 외 0이 저장되는 테이블
-tbl = [0] * 128  ## ASCII code 영어 소문자 최대값(97~122)
-for ch in "aeiou":
-    tbl[ord(ch)] = 1
-
-
+# DFS 시작
 dfs(0, 0, "")
-for st in ans:
-    print(st)
+
+# 결과 출력
+for password in ans:
+    print(password)
